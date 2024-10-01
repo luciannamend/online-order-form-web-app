435f7bca60eea9e906f075aef051106d3713a993
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-01 19:51:10 -0400

    Initial config

---
+++ src/main/java/com/lucianna/mendonca/onlineorderformwebapp/OnlineOrderFormWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.onlineorderformwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderFormWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOrderFormWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucianna/mendonca/onlineorderformwebapp/OnlineOrderFormWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.onlineorderformwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrderFormWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
