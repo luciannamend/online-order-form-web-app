90a01ddaebe7c216b61fa6c6a615b6f4cf8c4ca3
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-06 19:40:50 -0400

    Minor changes for testing

--- src/main/java/com/lucianna/mendonca/onlineorderformwebapp/OnlineOrderFormWebAppApplication.java
+++ src/main/java/com/lucianna/mendonca/onlineorderformwebapp/OnlineOrderFormWebAppApplication.java
@@ -1,7 +1,14 @@
 package com.lucianna.mendonca.onlineorderformwebapp;
 
+import com.lucianna.mendonca.onlineorderformwebapp.model.Product;
+import com.lucianna.mendonca.onlineorderformwebapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class OnlineOrderFormWebAppApplication {
@@ -9,5 +16,4 @@ public class OnlineOrderFormWebAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineOrderFormWebAppApplication.class, args);
     }
-
 }
