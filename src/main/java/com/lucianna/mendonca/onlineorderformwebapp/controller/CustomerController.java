bddc271fb0e2e0aee371f907b3f55194e81c7e66
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-05 20:27:51 -0400

    Added orderform controller to handle requests and send for customer and order table. Small other changes to adapt

--- src/main/java/com/lucianna/mendonca/onlineorderformwebapp/controller/CustomerController.java
+++ src/main/java/com/lucianna/mendonca/onlineorderformwebapp/controller/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/smartphone-web-form")
+@RequestMapping("customer")
 public class CustomerController {
 
     @Autowired
--- src/main/java/com/lucianna/mendonca/onlineorderformwebapp/controller/OrderController.java
+++ src/main/java/com/lucianna/mendonca/onlineorderformwebapp/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.lucianna.mendonca.onlineorderformwebapp.controller;
 
+import com.lucianna.mendonca.onlineorderformwebapp.model.OrderModel;
+import com.lucianna.mendonca.onlineorderformwebapp.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping
+    public ResponseEntity<OrderModel> saveOrder(@ModelAttribute OrderModel order){
+        // save order
+        OrderModel savedOrder = orderRepository.save(order);
+        // return saved order
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
 }
